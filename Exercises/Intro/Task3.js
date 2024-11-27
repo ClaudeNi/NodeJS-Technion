c92b8dbaa5559ded8c7adde1be3c63f9873af482
Claude Ni <Claudenij2@gmail.com>
2024-11-27 16:08:12 +0200

    task 3 commit

---
+++ Exercises/Intro/Task3.js
@@ -0,0 +1,10 @@
+// array of numbers
+let arr = [1, 2, 3, 0, 4, 5, 6, 7, 8, 9, 10, 0];
+// count of zeroes
+let count = 0;
+
+// go over array arr and count zeroes
+for (let i = 0; i < arr.length; i++) count += arr[i] === 0 ? 1 : 0;
+
+// print number of zeroes found in arr
+console.log(count);

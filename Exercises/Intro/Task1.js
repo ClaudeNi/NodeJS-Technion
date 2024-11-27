7dcd4a3aa3d30b6669df260f0daaba94cb90337a
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2024-11-27 15:53:30 +0200

    Add files via upload

---
+++ Exercises/Intro/Task1.js
@@ -0,0 +1,15 @@
+const num = 7;
+
+Number(
+  (num % 2 === 0 && num % 3 !== 0 && num % 5 !== 0) ||
+    (num % 2 !== 0 && num % 3 === 0 && num % 5 !== 0) ||
+    (num % 2 !== 0 && num % 3 !== 0 && num % 5 === 0)
+) && console.log(1);
+
+Number(
+  (num % 2 === 0 && num % 3 === 0 && num % 5 !== 0) ||
+    (num % 2 === 0 && num % 3 !== 0 && num % 5 === 0) ||
+    (num % 2 !== 0 && num % 3 === 0 && num % 5 === 0)
+) && console.log(2);
+
+Number(num % 2 === 0 && num % 3 === 0 && num % 5 === 0) && console.log(3);
